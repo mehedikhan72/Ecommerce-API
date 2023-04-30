f37ebcd17bdfa4172cdf65bf6745d5f4f138605e
mehedi khan <lopingcard@gmail.com>
2023-05-01 02:10:36 +0600

    loading added in admin dashboard

--- EcommerceAPI/EcommerceAPI/settings.py
+++ EcommerceAPI/EcommerceAPI/settings.py
@@ -58,7 +58,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 4,
+    'PAGE_SIZE': 7,
 }
 SIMPLE_JWT = {
     # TODO: change to 5 minutes
